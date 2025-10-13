83322e8dd05223491c16ebe37d4a5bcfca826858
Richard Wohlbold <richard@rgwohlbold.de>
2025-10-13 16:25:39 +0200

    Remove unused imports in k3mm_triton.py

--- npbench/benchmarks/polybench/k3mm/k3mm_triton.py
+++ npbench/benchmarks/polybench/k3mm/k3mm_triton.py
@@ -1,6 +1,4 @@
-import triton
 import torch
-import triton.language as tl
 from npbench.infrastructure.triton_utilities import matmul
 
 def kernel(A: torch.Tensor, B: torch.Tensor, C: torch.Tensor, D: torch.Tensor):
