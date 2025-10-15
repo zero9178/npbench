396e635cd8dbdb07cb8eff3c392541b1d77003d8
Anna Szymkowiak <aszymkowiak@student.ethz.ch>
2025-10-15 20:25:10 +0200

    Fix atax

--- npbench/benchmarks/polybench/atax/atax_triton.py
+++ npbench/benchmarks/polybench/atax/atax_triton.py
@@ -2,6 +2,7 @@ import torch
 from npbench.infrastructure.triton_utilities import matmul
 
 def kernel(A: torch.Tensor, x: torch.Tensor):
-    A_T = A.t()
+    A_T = A.t().contiguous()
+    x = x.view(-1, 1).t().contiguous()
     res = matmul(A_T, A)
-    return matmul(res, x)
+    return matmul(x, res)
