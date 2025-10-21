7490fe5f1331617490ea9596889638c295d3cbdf
Richard Wohlbold <richard@rgwohlbold.de>
2025-10-21 12:15:52 +0200

    Fuse kernels for more brrr

--- npbench/benchmarks/polybench/bicg/bicg_triton.py
+++ npbench/benchmarks/polybench/bicg/bicg_triton.py
@@ -4,78 +4,43 @@ import torch
 import triton
 import triton.language as tl
 
-
-def generate_config0():
+def generate_config():
     return [
-        triton.Config(kwargs={"BLOCK_SIZE_N": b, "BLOCK_SIZE_K": k}, num_warps=w)
-        for b, k, w in itertools.product(
+        triton.Config(kwargs={"BLOCK_SIZE_M": m, "BLOCK_SIZE_N": n}, num_warps=w)
+        for m, n, w in itertools.product(
             [8, 16, 32, 64, 128], [8, 16, 32, 64, 128], [1, 2, 4, 8]
         )
-        if b != 128 or k != 128
+        if m != 128 or n != 128
     ]
 
-
-@triton.autotune(configs=generate_config0(), key=["K", "N"], cache_results=True)
+@triton.autotune(configs=generate_config(), key=["M", "N"], cache_results=True)
 @triton.jit()
-def _kernel0(
-    A,  # (K, N)
-    X,  # (K, ),
-    out,  # (N, ),
-    K: tl.constexpr,
-    N: tl.constexpr,
-    BLOCK_SIZE_K: tl.constexpr,
-    BLOCK_SIZE_N: tl.constexpr,
-):
-    i = tl.program_id(axis=0)
-    j = tl.program_id(axis=1)
-
-    row = (i * BLOCK_SIZE_K) + tl.arange(0, BLOCK_SIZE_K)
-    column = (j * BLOCK_SIZE_N) + tl.arange(0, BLOCK_SIZE_N)
-    a = tl.load(
-        A + N * row[:, None] + column[None, :],
-        mask=(column[None, :] < N) & (row[:, None] < K),
-    )
-    X = tl.load(X + row, mask=row < K, other=0.0)
-
-    # Perform the reduction of the K dimension. A vector corresponding to an N tile remains.
-    a_sum = tl.sum(a * X[:, None], axis=0)
-    tl.atomic_add(out + column, a_sum, sem="release")
-
-
-def generate_config1():
-    return [
-        triton.Config(kwargs={"BLOCK_SIZE_M": b, "BLOCK_SIZE_K": k}, num_warps=w)
-        for b, k, w in itertools.product([8, 16, 32, 64, 128], [8, 16, 32, 64, 128], [1, 2, 4, 8])
-        if b != 128 or k != 128
-    ]
-
-
-@triton.autotune(configs=generate_config1(), key=["M", "K"], cache_results=True)
-@triton.jit()
-def _kernel1(
-    A,  # (M, K)
-    X,  # (K, ),
-    out,  # (M, ),
-    M: tl.constexpr,
-    K: tl.constexpr,
-    BLOCK_SIZE_M: tl.constexpr,
-    BLOCK_SIZE_K: tl.constexpr,
-):
-    zero = tl.zeros((BLOCK_SIZE_K,), out.dtype.element_ty)
+def _kernel(
+        A, # (M, N)
+        R, # (M, )
+        P, # (N, )
+        OUT0, # (M, )
+        OUT1, # (N, )
+        M: tl.constexpr,
+        N: tl.constexpr,
+        BLOCK_SIZE_M: tl.constexpr,
+        BLOCK_SIZE_N: tl.constexpr,
+    ):
     i = tl.program_id(axis=0)
     j = tl.program_id(axis=1)
 
     row = (i * BLOCK_SIZE_M) + tl.arange(0, BLOCK_SIZE_M)
-    column = (j * BLOCK_SIZE_K) + tl.arange(0, BLOCK_SIZE_K)
+    column = (j * BLOCK_SIZE_N) + tl.arange(0, BLOCK_SIZE_N)
     a = tl.load(
-        A + K * row[:, None] + column[None, :],
-        (column[None, :] < K) & (row[:, None] < M),
-    )
-    X = tl.load(X + column, mask=column < K, other=zero)
+        A + N * row[:, None] + column[None, :],
+        mask=(column[None, :] < N) & (row[:, None] < M))
+    r = tl.load(R + row, mask=row < M, other=0.0)
+    p = tl.load(P + column, mask=column < N, other=0.0)
 
-    # Perform the reduction of the K dimension. A vector corresponding to an M tile remains.
-    a_sum = tl.sum(a * X[None, :], axis=1)
-    tl.atomic_add(out + row, a_sum, sem="release")
+    r_sum = tl.sum(a * r[:, None], axis=0)
+    p_sum = tl.sum(a * p[None, :], axis=1)
+    tl.atomic_add(OUT0 + column, r_sum, sem="release")
+    tl.atomic_add(OUT1 + row, p_sum, sem="release")
 
 
 def kernel(A: torch.Tensor, p: torch.Tensor, r: torch.Tensor):
@@ -83,14 +48,9 @@ def kernel(A: torch.Tensor, p: torch.Tensor, r: torch.Tensor):
     out0 = torch.zeros((A.shape[1],), dtype=A.dtype)
     out1 = torch.zeros((A.shape[0],), dtype=A.dtype)
 
-    grid0 = lambda meta: (
-        triton.cdiv(A.shape[0], meta["BLOCK_SIZE_K"]),
+    grid = lambda meta: (
+        triton.cdiv(A.shape[0], meta["BLOCK_SIZE_M"]),
         triton.cdiv(A.shape[1], meta["BLOCK_SIZE_N"]),
     )
-    grid1 = lambda meta: (
-        triton.cdiv(A.shape[0], meta["BLOCK_SIZE_M"]),
-        triton.cdiv(A.shape[1], meta["BLOCK_SIZE_K"]),
-    )
-    _kernel0[grid0](A, r, out0, A.shape[0], A.shape[1])
-    _kernel1[grid1](A, p, out1, A.shape[0], A.shape[1])
+    _kernel[grid](A, r, p, out0, out1, A.shape[0], A.shape[1])
     return out0, out1
