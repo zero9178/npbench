d035367191567e9f1c03113af16f5ad4605301ce
josh-freeman <freemanjosh1000@gmail.com>
2025-10-29 15:56:56 +0100

    [softmax] No autotuning extremely simple version still yields 1000x speedup over numpy and 6x speedup over DaCe

---
+++ npbench/benchmarks/deep_learning/softmax/softmax_triton.py
@@ -0,0 +1,36 @@
+import torch
+import triton
+import triton.language as tl
+
+"""
+We will read the 4d tensor as a 2d matrix. 
+As far as the kernel is concerned, it's just
+like we had X*H*SM rows of SM elements to process. 
+"""
+@triton.jit
+def _kernel(x_ptr, n_rows, n_cols, BLOCK_SIZE:tl.constexpr):
+    row_idx = tl.program_id(0)
+
+    row_start_ptr = x_ptr + row_idx * n_cols
+    col_offsets = tl.arange(0, BLOCK_SIZE)
+    row_ptrs = row_start_ptr + col_offsets
+    mask = col_offsets < n_cols
+    row = tl.load(row_ptrs, mask=mask, other=-float("inf"))
+
+    row_max = tl.max(row) # will need to be accumulated
+
+    numerator = tl.exp(row-row_max) # will need to be accumulated in a second loop probably, somehow
+    sum = tl.sum(numerator, axis=0)
+
+    output = numerator/sum
+    tl.store(row_ptrs, output, mask=mask)
+
+def softmax(x:torch.Tensor):
+    X, H, SM, _ = x.shape
+    x = x.contiguous()
+    n_rows = X*H*SM
+    n_cols = SM
+    grid = (n_rows,)
+    BLOCK_SIZE = 512
+    _kernel[grid](x,n_rows,n_cols,BLOCK_SIZE=tl.constexpr(BLOCK_SIZE))
+    return x
