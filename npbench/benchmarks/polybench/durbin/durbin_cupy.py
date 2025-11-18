3a2163ed032b666d399ace139dd63ef9348797db
Joshua Freeman <47647715+josh-freeman@users.noreply.github.com>
2025-11-18 15:38:34 +0100

    Update durbin_cupy.py

--- npbench/benchmarks/polybench/durbin/durbin_cupy.py
+++ npbench/benchmarks/polybench/durbin/durbin_cupy.py
@@ -10,8 +10,8 @@ def kernel(r):
 
     for k in range(1, r.shape[0]):
         beta *= 1.0 - alpha * alpha
-        alpha = -(r[k] + np.dot(np.flip(r[:k]), y[:k])) / beta # set alpha to (-1/(beta)) * sum ( in[k], cdot(flipped in up to k, out up to k)). Use reverse indexing and blocking.
-        y[:k] += alpha * np.flip(y[:k]) # flip first k values of output and multiply by alpha. Could be done without flipping using reverse indexing + smart blocking.
-        y[k] = alpha # can be parallelized with the above operation
+        alpha = -(r[k] + np.dot(np.flip(r[:k]), y[:k])) / beta
+        y[:k] += alpha * np.flip(y[:k]) 
+        y[k] = alpha 
 
     return y
