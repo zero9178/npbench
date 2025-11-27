65e0586ba37e8e56e9744497a1a48b11d75623d1
josh-free <freemanjosh1000@gmail.com>
2025-11-27 13:19:41 +0100

    feat: fix durbin

--- npbench/benchmarks/polybench/durbin/durbin_triton.py
+++ npbench/benchmarks/polybench/durbin/durbin_triton.py
@@ -2,24 +2,16 @@ import triton
 import triton.language as tl
 import torch
 
-@triton.autotune(
-    configs=[
-        triton.Config({
-            "BLOCK_SIZE": bs,
-        }, num_warps=nw)
-        for bs in [1024, 2048, 4096, 8192]
-        for nw in [1, 2, 4, 8]
-    ], key=["N"]
-)
 @triton.jit
 def durbin_kernel(
     y_ptr,
+    y_temp_ptr,
     r_ptr,
     N: tl.constexpr,
-    BLOCK_SIZE: tl.constexpr, 
+    BLOCK_SIZE: tl.constexpr,
 ):
     alpha = -tl.load(r_ptr)
-    beta = tl.cast(1.0, tl.float64)
+    beta = alpha * 0.0 + 1.0
     tl.store(y_ptr, alpha)
 
     j_block = tl.arange(0, BLOCK_SIZE)
@@ -27,25 +19,34 @@ def durbin_kernel(
         beta = beta * (1.0 - alpha * alpha)
         r_k = tl.load(r_ptr + k)
 
-        dot_product_sum = tl.cast(0.0, tl.float64)
+        dot_product_sum = alpha * 0.0
         for j_start in tl.range(0, k, BLOCK_SIZE):
             j = j_start + j_block         
             j_rev = (k - 1) - j           
             
             mask = j < k
-            r_vec = tl.load(r_ptr + j, mask=mask, other=0.0)
-            y_rev_vec = tl.load(y_ptr + j_rev, mask=mask, other=0.0)
-            dot_product_sum += tl.sum(r_vec * y_rev_vec, axis=0)
+            j_rev_clamped = tl.where(mask, j_rev, 0)
+            r_vec = tl.load(r_ptr + j_rev_clamped, mask=mask, other=0.0)
+            y_vec = tl.load(y_ptr + j, mask=mask, other=0.0)
+            dot_product_sum += tl.sum(r_vec * y_vec, axis=0)
             
         alpha = -(r_k + dot_product_sum) / beta
+
+        for j_start in tl.range(0, k, BLOCK_SIZE):
+            j = j_start + j_block
+            mask = j < k
+            y_val = tl.load(y_ptr + j, mask=mask, other=0.0)
+            tl.store(y_temp_ptr + j, y_val, mask=mask)
+
         for j_start in tl.range(0, k, BLOCK_SIZE):
             j = j_start + j_block
             j_rev = (k - 1) - j
-            
+
             mask = j < k
-            
-            y_old = tl.load(y_ptr + j, mask=mask, other=0.0)
-            y_rev_vec = tl.load(y_ptr + j_rev, mask=mask, other=0.0)
+            j_rev_clamped = tl.where(mask, j_rev, 0)
+
+            y_old = tl.load(y_temp_ptr + j, mask=mask, other=0.0)
+            y_rev_vec = tl.load(y_temp_ptr + j_rev_clamped, mask=mask, other=0.0)
             y_new = y_old + alpha * y_rev_vec
             tl.store(y_ptr + j, y_new, mask=mask)
             
@@ -53,11 +54,15 @@ def durbin_kernel(
 
 def kernel(r: torch.Tensor):
     N = r.shape[0]
-    y = torch.empty_like(r, dtype=torch.float64, device=r.device)
-    
+    y = torch.empty_like(r)
+    y_temp = torch.empty_like(r)
+    BLOCK_SIZE = 256
+
     durbin_kernel[(1,)](
         y,
+        y_temp,
         r,
         N,
+        BLOCK_SIZE,
     )
     return y
