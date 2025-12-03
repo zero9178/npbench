0b455ec98b1588916c3603723a3097c2b839e824
Richard Wohlbold <accounts+github@rgwohlbold.de>
2025-12-03 18:49:46 +0100

    Optimize `fdtd_2d` implementation (#91)
    This PR further optimizes the `fdtd_2d` implementation. It mainly fuses
    three kernels which do not have sequential data dependencies. This makes
    the implementation faster than DaCe.
    
    ## Benchmarks
    
    ### DaCe
    
    ```
    $ uv run run_benchmark.py -p paper -f dace_gpu -b fdtd_2d
    /home/rwohlbold/npbench/npbench/infrastructure/framework.py:5: UserWarning: pkg_resources is deprecated as an API. See https://setuptools.pypa.io/en/latest/pkg_resources.html. The pkg_resources package is slated for removal as early as 2025-11-30. Refrain from using this package or pin to Setuptools<81.
      import pkg_resources
    ***** Testing DaCe GPU with fdtd_2d on the paper dataset, datatype default *****
    NumPy - default - validation: 2219ms
    DaCe GPU - fusion - first/validation: 97ms
    DaCe GPU - fusion - fusion - validation: SUCCESS
    DaCe GPU - fusion - median: 93ms
    DaCe GPU - parallel - first/validation: 92ms
    DaCe GPU - parallel - parallel - validation: SUCCESS
    DaCe GPU - parallel - median: 92ms
    DaCe GPU - auto_opt - first/validation: 93ms
    DaCe GPU - auto_opt - auto_opt - validation: SUCCESS
    DaCe GPU - auto_opt - median: 92ms
    ```
    
    ### Triton
    
    ```
    $ uv run run_benchmark.py -p paper -f triton -b fdtd_2d
    /home/rwohlbold/npbench/npbench/infrastructure/framework.py:5: UserWarning: pkg_resources is deprecated as an API. See https://setuptools.pypa.io/en/latest/pkg_resources.html. The pkg_resources package is slated for removal as early as 2025-11-30. Refrain from using this package or pin to Setuptools<81.
      import pkg_resources
    ***** Testing Triton with fdtd_2d on the paper dataset, datatype default *****
    NumPy - default - validation: 2218ms
    Triton - default - first/validation: 9434ms
    Triton - default - default - validation: SUCCESS
    Triton - default - median: 79ms
    ```

--- npbench/benchmarks/polybench/fdtd_2d/fdtd_2d_triton.py
+++ npbench/benchmarks/polybench/fdtd_2d/fdtd_2d_triton.py
@@ -24,86 +24,51 @@ def get_2d_configs():
         )
     ]
 
-
-@triton.autotune(
-    configs=get_boundary_configs(),
-    key=["ny"],
-)
-@triton.jit
-def _kernel_set_boundary(ey_ptr, fict_val, ny, BLOCK_SIZE: tl.constexpr):
-    """Set ey[0, :] = fict_val"""
-    pid = tl.program_id(0)
-    offsets = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    mask = offsets < ny
-    tl.store(ey_ptr + offsets, fict_val, mask=mask)
-
-
 @triton.autotune(
     configs=get_2d_configs(),
     key=["nx", "ny"],
 )
 @triton.jit
-def _kernel_update_ey(ey_ptr, hz_ptr, nx, ny, BLOCK_SIZE_X: tl.constexpr, BLOCK_SIZE_Y: tl.constexpr):
-    """Update ey[1:, :] -= 0.5 * (hz[1:, :] - hz[:-1, :])"""
+def _kernel_update_fields_fused(
+    ex_ptr, ey_ptr, fict_val, hz_ptr, 
+    nx, ny, 
+    BLOCK_SIZE_X: tl.constexpr, BLOCK_SIZE_Y: tl.constexpr
+):
     pid_x = tl.program_id(0)
     pid_y = tl.program_id(1)
 
-    x_base = pid_x * BLOCK_SIZE_X + 1
+    x_base = pid_x * BLOCK_SIZE_X
     y_base = pid_y * BLOCK_SIZE_Y
 
     x_offsets = x_base + tl.arange(0, BLOCK_SIZE_X)
     y_offsets = y_base + tl.arange(0, BLOCK_SIZE_Y)
 
+    # General Bounds
     x_mask = x_offsets < nx
     y_mask = y_offsets < ny
-
+    
     # Broadcast to 2D
     offsets_2d = x_offsets[:, None] * ny + y_offsets[None, :]
-    mask_2d = x_mask[:, None] & y_mask[None, :]
+    general_mask = x_mask[:, None] & y_mask[None, :]
 
-    hz_curr = tl.load(hz_ptr + offsets_2d, mask=mask_2d, other=0.0)
-    hz_prev = tl.load(hz_ptr + offsets_2d - ny, mask=mask_2d, other=0.0)
+    hz_curr = tl.load(hz_ptr + offsets_2d, mask=general_mask, other=0.0)
+    ex_curr = tl.load(ex_ptr + offsets_2d, mask=general_mask, other=0.0)
+    ey_curr = tl.load(ey_ptr + offsets_2d, mask=general_mask, other=0.0)
 
-    # Load current ey and update
-    ey_curr = tl.load(ey_ptr + offsets_2d, mask=mask_2d, other=0.0)
-    ey_new = ey_curr - 0.5 * (hz_curr - hz_prev)
+    # Create a mask that is true only if row > 0 to prevent wrap-around
+    has_top_neighbor = (x_offsets[:, None] > 0) & general_mask
+    hz_top = tl.load(hz_ptr + offsets_2d - ny, mask=has_top_neighbor, other=0.0)
+    ey_update = ey_curr - 0.5 * (hz_curr - hz_top)
+    ey_new = tl.where(x_offsets[:, None] == 0, fict_val, ey_update)
+    
+    # Create a mask that is true only if col > 0 to prevent wrap-around
+    has_left_neighbor = (y_offsets[None, :] > 0) & general_mask
+    hz_left = tl.load(hz_ptr + offsets_2d - 1, mask=has_left_neighbor, other=0.0)
+    ex_new_val = ex_curr - 0.5 * (hz_curr - hz_left)
+    
+    tl.store(ex_ptr + offsets_2d, ex_new_val, mask=has_left_neighbor)
+    tl.store(ey_ptr + offsets_2d, ey_new, mask=general_mask)
 
-    tl.store(ey_ptr + offsets_2d, ey_new, mask=mask_2d)
-
-
-@triton.autotune(
-    configs=get_2d_configs(),
-    key=["nx", "ny"],
-)
-@triton.jit
-def _kernel_update_ex(ex_ptr, hz_ptr, nx, ny, BLOCK_SIZE_X: tl.constexpr, BLOCK_SIZE_Y: tl.constexpr):
-    """Update ex[:, 1:] -= 0.5 * (hz[:, 1:] - hz[:, :-1])"""
-    pid_x = tl.program_id(0)
-    pid_y = tl.program_id(1)
-
-    # Process columns 1 to ny-1
-    x_base = pid_x * BLOCK_SIZE_X
-    y_base = pid_y * BLOCK_SIZE_Y + 1
-
-    x_offsets = x_base + tl.arange(0, BLOCK_SIZE_X)
-    y_offsets = y_base + tl.arange(0, BLOCK_SIZE_Y)
-
-    x_mask = x_offsets < nx
-    y_mask = y_offsets < ny
-
-    # Broadcast to 2D
-    offsets_2d = x_offsets[:, None] * ny + y_offsets[None, :]
-    mask_2d = x_mask[:, None] & y_mask[None, :]
-
-    # Load hz[i, j] and hz[i, j-1]
-    hz_curr = tl.load(hz_ptr + offsets_2d, mask=mask_2d, other=0.0)
-    hz_prev = tl.load(hz_ptr + offsets_2d - 1, mask=mask_2d, other=0.0)
-
-    # Load current ex and update
-    ex_curr = tl.load(ex_ptr + offsets_2d, mask=mask_2d, other=0.0)
-    ex_new = ex_curr - 0.5 * (hz_curr - hz_prev)
-
-    tl.store(ex_ptr + offsets_2d, ex_new, mask=mask_2d)
 
 
 @triton.autotune(
@@ -146,20 +111,13 @@ def _kernel_update_hz(hz_ptr, ex_ptr, ey_ptr, nx, ny, BLOCK_SIZE_X: tl.constexpr
 def kernel(TMAX, ex, ey, hz, _fict_):
     nx, ny = ex.shape
 
-    grid_boundary = lambda meta: (triton.cdiv(ny, meta['BLOCK_SIZE']),)
-    grid_2d_ey = lambda meta: (triton.cdiv(nx - 1, meta['BLOCK_SIZE_X']), triton.cdiv(ny, meta['BLOCK_SIZE_Y']))
-    grid_2d_ex = lambda meta: (triton.cdiv(nx, meta['BLOCK_SIZE_X']), triton.cdiv(ny - 1, meta['BLOCK_SIZE_Y']))
+    grid_2d_ey = lambda meta: (triton.cdiv(nx, meta['BLOCK_SIZE_X']), triton.cdiv(ny, meta['BLOCK_SIZE_Y']))
     grid_2d_hz = lambda meta: (triton.cdiv(nx - 1, meta['BLOCK_SIZE_X']), triton.cdiv(ny - 1, meta['BLOCK_SIZE_Y']))
 
+    fict_vals = _fict_.cpu().numpy()
     for t in range(TMAX):
-        # Set boundary
-        _kernel_set_boundary[grid_boundary](ey, _fict_[t].item(), ny)
-
         # Update ey
-        _kernel_update_ey[grid_2d_ey](ey, hz, nx, ny)
-
-        # Update ex
-        _kernel_update_ex[grid_2d_ex](ex, hz, nx, ny)
+        _kernel_update_fields_fused[grid_2d_ey](ex, ey, float(fict_vals[t]), hz, nx, ny)
 
         # Update hz
         _kernel_update_hz[grid_2d_hz](hz, ex, ey, nx, ny)
